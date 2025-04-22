a746d0a3370bf4df829c4304339a351ca6a67405
teddy-sio-slam <xeshing@proton.me>
2025-04-22 14:09:38 +0200

    tt3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-const SERVER_PORT = 3000;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = 'https://teddy-sio-slam.github.io';
-
-// Détection du mode
-const isBuild = import.meta.env.MODE === "production";
-const BASE_URL = isBuild ? LIVE_URL : LOCALHOST_URL;
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-	integrations: [tailwind()],
-	site: LIVE_URL,
-	base: isBuild ? '/teddy-sio-slam/' : '/', // 💡 chemin relatif utilisé par GitHub Pages
-});
+  site: 'https://teddy-sio-slam.github.io',
+  // Pas besoin de base dans ce cas
+})
