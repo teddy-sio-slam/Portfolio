f001b2a5b1a4bd846b29dd6a3ef8dd910ecc2556
teddy-sio-slam <xeshing@proton.me>
2025-04-22 13:57:01 +0200

    tt

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 	integrations: [tailwind()],
 	site : 'https://teddy-sio-slam.github.io/Portfolio/',
-	base : 'Portfolio',
+	base : '/Portfolio/',
 });
