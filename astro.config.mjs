505236b9a651a78cce07148a10ae6e41b1b250bb
teddy-sio-slam <xeshing@proton.me>
2025-04-22 14:39:17 +0200

    tttttt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://teddy-sio-slam.github.io',
-	base: '/retrograde-remnant/'
+	site: 'https://teddy-sio-slam.github.io'
 });
