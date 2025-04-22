1d0de8caec96a5f5f0c1b2f97d53a218dceb7eec
teddy-sio-slam <xeshing@proton.me>
2025-04-22 14:05:49 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = 'https://teddy-sio-slam.github.io';
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+	BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site : 'https://teddy-sio-slam.github.io/Portfolio/',
-	base : 'Portfolio/',
+	site : LIVE_URL,
+	base : BASE_URL,
 });
